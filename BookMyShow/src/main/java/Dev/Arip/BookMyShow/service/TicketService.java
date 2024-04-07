79707ed19e3b6ec7e1f3b27331b6044acc48406a
arip111 <150458760+arip111@users.noreply.github.com>
2024-04-07 11:59:29 +0530

    added our First API

---
+++ BookMyShow/src/main/java/Dev/Arip/BookMyShow/service/TicketService.java
@@ -0,0 +1,11 @@
+package Dev.Arip.BookMyShow.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TicketService {
+    public String greet()
+    {
+        return "Hello";
+    }
+}
